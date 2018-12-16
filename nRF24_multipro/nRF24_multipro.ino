784dabf6431a0605e0c61a46136b15f01334f316
Goebish <goebish@gmail.com>
2018-12-16 11:57:06 +0100

    Fix build with latest Arduino IDE release

--- nRF24_multipro/nRF24_multipro.ino
+++ nRF24_multipro/nRF24_multipro.ino
@@ -1,36 +1,34 @@
-/*
- ##########################################
- #####   MultiProtocol nRF24L01 Tx   ######
- ##########################################
- #        by goebish on rcgroups          #
- #                                        #
- #   Parts of this project are derived    #
- #     from existing work, thanks to:     #
- #                                        #
- #   - PhracturedBlue for DeviationTX     #
- #   - victzh for XN297 emulation layer   #
- #   - Hasi for Arduino PPM decoder       #
- #   - hexfet, midelic, closedsink ...    #
- ##########################################
-
-
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License.
- If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include <util/atomic.h>
 #include <EEPROM.h>
 #include "iface_nrf24l01.h"
+
+// ##########################################
+// #####   MultiProtocol nRF24L01 Tx   ######
+// ##########################################
+// #        by goebish on rcgroups          #
+// #                                        #
+// #   Parts of this project are derived    #
+// #     from existing work, thanks to:     #
+// #                                        #
+// #   - PhracturedBlue for DeviationTX     #
+// #   - victzh for XN297 emulation layer   #
+// #   - Hasi for Arduino PPM decoder       #
+// #   - hexfet, midelic, closedsink ...    #
+// ##########################################
+//
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License.
+// If not, see <http://www.gnu.org/licenses/>.
 
 
 // ############ Wiring ################
